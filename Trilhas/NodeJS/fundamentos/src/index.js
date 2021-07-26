fb7073982ff7792c97f210f8547b6aeb8561d9e9
Gabriel F. Vilar <gabriel@cogumm.net>
2021-07-26 18:09:42 -0300

    NodeJS: Chapter I - Tipos de parâmetros

--- Trilhas/NodeJS/fundamentos/src/index.js
+++ Trilhas/NodeJS/fundamentos/src/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 
+/**
+ * Instâncianado a aplicação utilizando o express.
+ */
 const app = express();
+
+/**
+ * Utilizando o express para converter o JSON em objeto do JS/TS.
+ */
+app.use(express.json());
 
 /**
  * GET - Buscar uma informação dentro do servidor.
@@ -10,15 +18,31 @@ const app = express();
  * DELETE - Deletar uma informação dentro do servidor.
  */
 
+/**
+ * Tipos de parâmetros.
+ *
+ * Router Params => Identificar um recurso listar/editar/deletar.
+ * Query Params => Paginação / filtros de busca.
+ * Body Params => Objetos nas inserções / alterações (JSON)
+ */
+
 app.get("/courses", (req, res) => {
+  const query = req.query;
+  console.log(query);
   return res.json(["Curso 1", "Curso 2", "Curso 3"]);
 });
 
 app.post("/courses", (req, res) => {
+  const body = req.body;
+
+  console.log(body);
   return res.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
 });
 
 app.put("/courses/:id", (req, res) => {
+  const { id } = req.params;
+
+  console.log(id);
   return res.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
 });
 
