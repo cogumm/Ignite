be1dc1d859a2c2cc7018f83c7618cf3f0740e40b
Gabriel F. Vilar <cogumm@gmail.com>
2022-02-07 16:39:52 -0300

    Chapter I - Servindo HTML estático

--- Trilhas/ReactJS/GithubExplorer/webpack.config.js
+++ Trilhas/ReactJS/GithubExplorer/webpack.config.js
@@ -1,4 +1,5 @@
-const resolve = require("path");
+const { resolve } = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: 'development',
@@ -10,6 +11,11 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx'],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: resolve(__dirname, 'public', 'index.html')
+    })
+  ],
   module: {
     rules: [
       {
